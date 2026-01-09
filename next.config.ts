91700a6ad1aa0d1458bfb7842a62c21f4e57d030
Nikhil S <nikhilsnayak3473@gmail.com>
2026-01-09 15:07:32 +0530

    cache components

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
   typedRoutes: true,
+  reactCompiler: true,
+  cacheComponents: true,
   typescript: {
     ignoreBuildErrors: true,
   },
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader2 } from 'lucide-react';
+
+export default function Loading() {
+  return (
+    <div className='grid min-h-dvh w-full place-items-center'>
+      <Loader2 className='animate-spin text-5xl' />
+    </div>
+  );
+}
